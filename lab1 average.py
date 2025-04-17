d4f3851d3f037182cdf3548d74dcc107e547b782
harshvasani09 <harshvasani05@gmail.com>
2025-04-17 23:13:33 +0530

    lab1 average.py

---
+++ lab1 average.py
@@ -0,0 +1,6 @@
+a = float(input("Enter marks of subject 1 (a) = "))
+b = float(input("Enter marks of subject 2 (b) = "))
+c = float(input("Enter marks of subject 3 (c) = "))
+total = a + b + c
+print("Total = ", total)
+print("Average = ", total / 3)
