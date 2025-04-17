bd74c18de8d999b833234959dca7fbb046b17520
harshvasani09 <harshvasani05@gmail.com>
2025-04-18 00:18:06 +0530

    Create lab5 uppercase conversion.py

---
+++ lab5 uppercase conversion.py
@@ -0,0 +1,9 @@
+string_list = ["hello", "world", "python", "programming", "fun"]
+
+print("Original list of strings:")
+print(string_list)
+
+uppercase_list = [word.upper() for word in string_list]
+
+print("\nList after converting all strings to uppercase:")
+print(uppercase_list)
