a078457148ca13f1b160c45353900ac7b7417e43
harshvasani09 <harshvasani05@gmail.com>
2025-04-17 23:11:56 +0530

    lab1 amsd.py

---
+++ lab1 amsd.py
@@ -0,0 +1,6 @@
+a = float(input("Enter first number (a) = "))
+b = float(input("Enter second number (b) = "))
+print("Addition = ", a + b)
+print("Subtraction = ", a - b)
+print("Multiplication = ", a * b)
+print("Division = ", a / b if b != 0 else "Cannot divide by zero")
