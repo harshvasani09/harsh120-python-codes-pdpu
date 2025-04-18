a586a42a395edd6f7c06fab30947a57898c842bc
harshvasani09 <harshvasani05@gmail.com>
2025-04-18 21:42:30 +0530

    Delete lab q2.py

--- lab q2.py
+++
@@ -1,8 +0,0 @@
-def compute(n):
-    n_str = str(n)
-    result = int(n_str) + int(n_str * 2) + int(n_str * 3) + int(n_str * 4)
-    return result
-
-for digit in range(4, 8):
-    result = compute(digit)
-    print(f"Result for {digit}: {result}")
