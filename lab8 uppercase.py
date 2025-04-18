4485b78546b30576479f1cc0db2aefa84e81f040
harshvasani09 <harshvasani05@gmail.com>
2025-04-18 21:29:23 +0530

    Create lab8 uppercase.py

---
+++ lab8 uppercase.py
@@ -0,0 +1,6 @@
+items_list = ["mango", "carrot", "potato", "mango", "spinach", "carrot"]
+uppercase_set = set()
+for item in items_list:
+    uppercase_set.add(item.upper())
+
+print("Set of uppercase items:", uppercase_set)
