6fce39bbab161f564112c3230d5c0bbe97e081b1
harshvasani09 <harshvasani05@gmail.com>
2025-04-18 21:35:47 +0530

    Create lab8 A AND B.py

---
+++ lab8 A AND B.py
@@ -0,0 +1,8 @@
+names = {"Ravi", "Meera", "Vikram", "Sita", "Babu"}
+
+r_set = {n for n in names if n.startswith("R")}
+
+s_set = {n for n in names if n.startswith("S")}
+
+print("R:", r_set)
+print("S:", s_set)
